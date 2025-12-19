7f1b8b8eff8a78a9c76d18395dc0bfd9a7a954cf
Agam <agamv2009@gmail.com>
2025-12-19 16:54:49 +0000

    test

--- Scenes/bird.gd
+++ Scenes/bird.gd
@@ -6,3 +6,15 @@ const FLAP_SPEED : int = -500
 var flying : bool = false
 var falling : bool = false
 const START_POS = Vector2(100, 400)
+
+# Sets up the scene for when first started
+func _ready():
+	reset()
+	
+func reset():
+	falling = false
+	flying = false
+	position = START_POS
+	set_rotation(0)
+	
+#Called every frame, Delta is time passed since last frame
